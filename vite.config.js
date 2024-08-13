eba0dae6e7866d227df040c6e2c3abe6987f5180
Pratik Mali <pratikmali242005@gmail.com>
2024-08-13 23:23:28 +0530

    v2.0

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'dist', 
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
   },
 });
