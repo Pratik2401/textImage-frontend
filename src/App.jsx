fa77a95659066016dc6c21b55026832eea4c21d1
Pratik Mali <pratikmali242005@gmail.com>
2024-08-14 11:24:23 +0530

    analystics

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TextArea from './components/TextArea';
 import MessagePage from './components/MessagePage'; // Import the new component
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
+    <>
+    <Analytics/>
     <Router>
       <Routes>
         <Route path="/" element={<TextArea />} />
         <Route path="/:id" element={<MessagePage />} /> 
       </Routes>
     </Router>
+    </>
   );
 }
 
